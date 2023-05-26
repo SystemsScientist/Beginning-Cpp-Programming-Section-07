51136c3ba74a7936c2bbe5f835f455c0e2b6382f
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-26 06:16:57 -0500

    Adding 56-Accessing-and-Modifying-Array-Elements files and directories

---
+++ Section-07-Workspace/56-Accessing-and-Modifying-Array-Elements/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: arrays
+ *  Purpose:
+ *  Date: 05/26/2023 
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    char vowels [] {'a', 'e', 'i', 'o', 'u'};
+    cout << "\nThe first vowel is: " << vowels[0] << endl;
+    cout << "The last vowel is: " << vowels[4] << endl;
+    
+    return 0;
+}
