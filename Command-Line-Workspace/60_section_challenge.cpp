9e8ce8d2171aa89369b5cd229e03bdfb1ff7ce4a
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-29 06:19:49 -0500

    Adding 60_section_challenge.cpp file

---
+++ Command-Line-Workspace/60_section_challenge.cpp
@@ -0,0 +1,48 @@
+/*
+ * Author: Matt Johnson
+ * Subject: arrays and vectors
+ * Purpose: Illustrates section challenge
+ * Date: 05/29/2023
+ *
+ * Instructions:
+ *
+ * 	Declare 2 empty vectors of integers named vector1 and vector2, respectively
+ *
+ * 	Add 10 and 20 to vector1 dynamically using push_back
+ * 	Display the elements in vector1 using the at() method as well as its size
+ * 	using the size() method
+ *
+ * 	Add 100 and 200 to vector2 dynamically using push_back
+ * 	Display the elements in vector2 using the at() method as well as its size
+ * 	using the size() method
+ *
+ * 	Declare an empty 2D vector called vector_2d
+ * 	Remember, that a 2D vector is a vector of vector <int>
+ *
+ * 	Add vector1 to vector_2d dynamically using push_back
+ * 	Add vector2 to vector_2d dynamically using push_back
+ *
+ * 	Display the elements in vector_2d using the at() method
+ *
+ * 	Change vector1.at(0) to 1000
+ *
+ * 	Display the elements in vector_2d again using the at() method
+ *
+ * 	Display the elements in vector1
+ *
+ * 	What did you expect? Did you get what you expected? What do you think
+ * 	happened?
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+
+	cout << "This program illustrates the requirements" << endl;
+	cout << "for the Chapter 7 challenge." << endl;
+
+	return 0;
+}
